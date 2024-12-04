a23f0256b6f3146d4bec43d409f8927ceeb1f8d4
BhagYagam <bhagyagmge@gmail.com>
2024-12-04 14:52:48 +0530

    manage item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AddCustomerPageComponent } from './page/add-customer-page/add-customer-page.component';
 import { ManageCustomerPageComponent } from './page/manage-customer-page/manage-customer-page.component';
 import { AddItemPageComponent } from './page/add-item-page/add-item-page.component';
+import { ManageItemPageComponent } from './page/manage-item-page/manage-item-page.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path:"app-add-item-page",
         component:AddItemPageComponent
+    },
+    {
+        path:"app-manage-item-page",
+        component:ManageItemPageComponent
     }
 ];
--- src/app/page/manage-item-page/manage-item-page.component.ts
+++ src/app/page/manage-item-page/manage-item-page.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-manage-item-page',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './manage-item-page.component.html',
   styleUrl: './manage-item-page.component.css'
 })
 export class ManageItemPageComponent {
+
+  public itemList:any=[];
+
+  constructor(private http:HttpClient){
+    this.loadTable();
+  }
+
+  loadTable(){
+      this.http.get("http://localhost:8080/item/get-all").subscribe(data=>{
+        
+        this.itemList=data;
+        console.log(this.itemList);
+
+      })
+  }
+
+  deleteItemById(id:any){
+    console.log(id);
+    this.http.delete(`http://localhost:8080/item/delete-by-id/${id}`).subscribe(data=>{
+      alert("deleted!")
+      // this.alertDelete();
+      this.loadTable();
+      
+     })
+}
+
+public itemTemp:any={}
+
+
+updateItem(item:any){
+  console.log(item);
+  this.itemTemp=item;
+}
+
+saveItem(){
+  this.http.put("http://localhost:8080/item/update-item ",this.itemTemp).subscribe(data=>{
+    console.log(this.itemTemp);
+    alert("updated!");
+    // this.alertLoginSuccessful();
+  })
+}
 
 }
